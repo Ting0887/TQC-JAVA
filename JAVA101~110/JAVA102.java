ac7944c94d3baa56eb700e937615c6963c472655
Ting0887 <57065036+Ting0887@users.noreply.github.com>
2020-06-05 22:46:21 +0800

    Add files via upload

---
+++ JAVA101~110/JAVA102.java
@@ -0,0 +1,16 @@
+//102 ³æ¦ì´«ºâ 
+import java.util.Scanner;
+public class JAVA102 {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Please input:");
+		double kilo,pound;
+		kilo = scanner.nextDouble();
+		pound = kilo*2.20462;
+		System.out.printf("%f kg = %f ponds\n",kilo,pound);
+	}
+
+}
